5905043bbdfb3e337f5039f3d719ab6f863e3ec6
jonalger5 <jonayer123@gmail.com>
2018-01-30 15:15:49 -0700

    Added Basic Inventory System

---
+++ TheEscape/Assets/Scripts/Item.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Item {
+
+    public enum ItemType
+    {
+        Consumable,
+        Valuable,
+        Other
+    }
+
+    public string name;
+    public ItemType type;
+    public float value;
+    public Texture2D icon;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- TheEscape/Assets/Scripts/PlayerController.cs
+++ TheEscape/Assets/Scripts/PlayerController.cs
@@ -11,16 +11,43 @@ public class PlayerController : MonoBehaviour {
     public float health;
     private Text healthUI;
 
-    private Rigidbody rb;
     private Renderer renderer;
+
+    public List<Item> inventory;
+    private int slotX = 4, slotY = 4;
+    //private int[] slots;
+    private ItemDatabase database;
+    private bool showInventory = false;
+    private bool showItem = false;
+    private string ItemDetails;
 
     // Use this for initialization
     void Start () {
         healthUI = GameObject.Find("/HealthUI/Health").GetComponent<Text>();
-        rb = GetComponent<Rigidbody>();
         renderer = GetComponent<Renderer>();
         renderer.material.color = Color.yellow;
-	}
+        inventory = new List<Item>();
+        //slots = new int[slotX * slotY];
+        database = GameObject.Find("Item Database").GetComponent<ItemDatabase>();
+
+        //Used for testing inventory
+        //inventory.Add(database.Items[0]);
+        //inventory.Add(database.Items[1]);
+        //inventory.Add(database.Items[2]);
+        //inventory.Add(database.Items[3]);
+        //inventory.Add(database.Items[0]);
+        //inventory.Add(database.Items[1]);
+        //inventory.Add(database.Items[2]);
+        //inventory.Add(database.Items[3]);
+        //inventory.Add(database.Items[0]);
+        //inventory.Add(database.Items[1]);
+        //inventory.Add(database.Items[2]);
+        //inventory.Add(database.Items[3]);
+        //inventory.Add(database.Items[0]);
+        //inventory.Add(database.Items[1]);
+        //inventory.Add(database.Items[2]);
+        //inventory.Add(database.Items[3]);
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -32,5 +59,53 @@ public class PlayerController : MonoBehaviour {
         //Updating HealthUI
         healthUI.text = health.ToString();
 
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            showInventory = !showInventory;
+        }
+    }
+
+    void OnGUI()
+    {
+        ItemDetails = "";
+
+        if (showInventory)
+        {
+            for (int x = 0; x < slotX; x++)
+            {
+                for (int y = 0; y < slotY; y++)
+                {                    
+                    if (x * slotX + y + 1 <= inventory.Count)
+                    {
+                        Rect slot = new Rect(10 + y * 60, 50 + x * 60, 50, 50);
+                        GUI.Box(slot, inventory[x * slotX + y].icon);
+                        if (slot.Contains(Event.current.mousePosition))
+                        {
+                            ItemDetails = ShowItem(inventory[x * slotX + y]);
+                            showItem = true;
+                        }
+
+                        if (ItemDetails == "")
+                            showItem = false;
+                    }                    
+                }
+            }
+        }
+
+        if (showItem)
+        {
+            GUI.Box(new Rect(Event.current.mousePosition.x, Event.current.mousePosition.y, 150, 50), ItemDetails);
+        }
+    }
+
+    private string ShowItem(Item item)
+    {
+        if (item.type == Item.ItemType.Valuable)
+            ItemDetails = item.name + "\n" + "Type: Valuable\n Value: " + item.value;   
+
+        if (item.type == Item.ItemType.Consumable)
+            ItemDetails = item.name + "\n" + "Type: Consumable\n Health: " + item.value;
+
+        return ItemDetails;
     }
 }
