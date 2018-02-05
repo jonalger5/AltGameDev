f72f6741be14410ae17ab6e5858125df57cae83e
Alexander Wittmann <alexwittmann01@gmail.com>
2018-02-04 17:35:08 -0700

    Added SortingMiniGame
    Added the base for sorting minigame scene

--- TheEscape/Assets/Scripts/PlayerController.cs
+++ TheEscape/Assets/Scripts/PlayerController.cs
@@ -25,6 +25,17 @@ public class PlayerController : MonoBehaviour {
     public Texture2D EmptySlot;
     public GUISkin slotBackground;
 
+    public List<Item> Pickup;
+    private bool onPickup = false;
+    private int itemnum;
+    private int quantity;
+    private bool showPickup = false;
+    
+    private bool redpickup = false;
+    private bool bluepickup = false;
+    private bool yellowpickup = false;
+    private bool greenpickup = false;
+
     // Use this for initialization
     void Start () {
         healthUI = GameObject.Find("/HealthUI/Health").GetComponent<Text>();
@@ -32,6 +43,8 @@ public class PlayerController : MonoBehaviour {
         renderer.material.color = Color.yellow;
         inventory = new List<Item>();
         database = GameObject.Find("Item Database").GetComponent<ItemDatabase>();
+
+        Pickup = new List<Item>();
 
         //Used for testing inventory
         inventory.Add(database.Items[0]);
@@ -103,6 +116,101 @@ public class PlayerController : MonoBehaviour {
         if (showItem)
         {
             GUI.Box(new Rect(Event.current.mousePosition.x, Event.current.mousePosition.y, 150, 50), ItemDetails);
+        }
+
+        if (onPickup)
+        {
+            //GUI.Box(new Rect(10, 50, 240, 240), InventoryBackground);
+            for (int x = 0; x < 1; x++)
+            {
+                for (int y = 0; y < quantity; y++)
+                {
+                    if (x * slotX + y + 1 <= Pickup.Count)
+                    {
+                        Rect slot = new Rect(900 + y * 60, 20 + x * 60, 50, 50);
+                        GUI.Box(slot, Pickup[x * slotX + y].icon);
+                        if (slot.Contains(Event.current.mousePosition))
+                        {
+                            ItemDetails = ShowItem(Pickup[x * slotX + y]);
+                            showPickup = true;
+                        }
+
+                        if (ItemDetails == "")
+                            showPickup = false;
+                    }
+                    else
+                    {
+                        Rect slot = new Rect(10 + y * 60, 50 + x * 60, 50, 50);
+                        GUI.Box(slot, EmptySlot);
+                    }
+                }
+            }
+        }
+
+        if (showPickup)
+        {
+            GUI.Box(new Rect(Event.current.mousePosition.x, Event.current.mousePosition.y, 150, 50), ItemDetails);
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "PileOfItems" && !onPickup)
+        {
+
+            itemnum = Random.Range(0, 3);
+            quantity = Random.Range(1, 4);
+            onPickup = true;
+
+            if(itemnum == 0)
+            {
+                redpickup = true;
+            }
+
+            else if (itemnum == 1)
+            {
+                yellowpickup = true;
+            }
+
+            else if (itemnum == 2)
+            {
+                greenpickup = true;
+            }
+
+            else if (itemnum == 3)
+            {
+                bluepickup = true;
+            }
+
+            for (int i = 0;i <= quantity; i++)
+            {
+                Pickup.Add(database.Items[itemnum]);
+            }
+
+        }
+
+        if(other.gameObject.name == "RedPile" && onPickup && redpickup)
+        {
+            Pickup.Clear();
+            onPickup = false;
+        }
+
+        if (other.gameObject.name == "BluePile" && onPickup && bluepickup)
+        {
+            Pickup.Clear();
+            onPickup = false;
+        }
+
+        if (other.gameObject.name == "YellowPile" && onPickup && yellowpickup)
+        {
+            Pickup.Clear();
+            onPickup = false;
+        }
+
+        if (other.gameObject.name == "GreenPile" && onPickup && greenpickup)
+        {
+            Pickup.Clear();
+            onPickup = false;
         }
     }
 
