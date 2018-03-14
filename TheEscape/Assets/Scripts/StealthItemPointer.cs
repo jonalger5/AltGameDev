f9a24e02d195e966b009a02eb08cec454bd66a12
brandonmerrin <brandonmerrin@gmail.com>
2018-03-14 12:14:29 -0600

    Updated Indicator

--- TheEscape/Assets/Scripts/StealthItemPointer.cs
+++ TheEscape/Assets/Scripts/StealthItemPointer.cs
@@ -1,42 +1,54 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
 public class StealthItemPointer : MonoBehaviour {
 
     public GameObject player;
-    private PlayerController _player;
     public GameObject item;
+    private float prevHeadAngle;
+    private float prevAngle;
 
     // Use this for initialization
     void Start () {
-        _player = GameObject.Find("MainCharacter").GetComponent<PlayerController>();
+        prevHeadAngle = 360;
+        prevAngle = 0;
+    }
+
+    void UpdateAngle ()
+    {
+        var playerPos = player.transform.position;
+        var itemPosition = item.transform.position;
+        RectTransform rectTransform = GetComponent<RectTransform>();
+
+        float directZ = playerPos.z - itemPosition.z;
+        float directX = playerPos.x - itemPosition.x;
+        float angle = Mathf.Atan2(directZ, directX) * Mathf.Rad2Deg;
+
+        Vector3 forward = player.transform.forward;
+        forward.y = 0;
+        float headingAngle = Quaternion.LookRotation(forward).eulerAngles.y;
+        headingAngle = headingAngle - 270;
+        print("headingAngle: " + headingAngle);
+        print("prevAngle: " + prevHeadAngle);
+        print("angle: " + angle);
+        float angleToRotate = (angle - prevAngle);
+        if (angle <= 180)
+        {
+            angleToRotate -= (prevHeadAngle - headingAngle);
+        }
+        else
+        {
+            angleToRotate += (prevHeadAngle - headingAngle);
+        }
+        rectTransform.Rotate(new Vector3(0, 0, angleToRotate)); 
+        prevHeadAngle = headingAngle;
+        prevAngle = angle;
     }
 	
 	// Update is called once per frame
 	void Update () {
-        var playerPos = player.transform.position;
-        var itemPosition = item.transform.position;
-
-        Vector3 dir = playerPos - itemPosition;
-        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
-
-        // Vector3 forward = player.transform.forward;
-        //forward.y = 0;
-        // float headingAngle = Quaternion.LookRotation(forward).eulerAngles.y;
-
-        //RectTransform rectTransform = GetComponent<RectTransform>();
-        //float correctionAngle = rectTransform.eulerAngles.z - angle;
-        //rectTransform.Rotate(new Vector3(0, 0, angle));
-        
-
-        //Quaternion newAngle = Quaternion.FromToRotation(playerPos, itemPosition);
-        //rectTransform.SetPositionAndRotation(playerPos, newAngle);
-        //rectTransform.SetPositionAndRotation(itemPosition, angle);
-       
-        // new Quaternion(rotation.x, 0, rotation.z, rotation.w)
-
-        Quaternion rotation = Quaternion.LookRotation(_player.transform.position - item.transform.position);
-        transform.rotation = Quaternion.RotateTowards(transform.rotation, rotation, 10);
+        UpdateAngle();
     }
 }
