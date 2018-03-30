0ce8340cdf0f85a1d75362c98046f667d35f1ce8
brandonmerrin <brandonmerrin@gmail.com>
2018-03-30 16:19:02 -0600

    Guards

---
+++ TheEscape/Assets/Scripts/CameraSwitching.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSwitching : MonoBehaviour
+{
+
+    public Camera mainCamera;
+    public Camera alternateCamera;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        mainCamera.enabled = false;
+        alternateCamera.enabled = true;
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        alternateCamera.enabled = false;
+        mainCamera.enabled = true;
+    }
+
+    public void OnTriggerStay(Collider other)
+    {
+        if (mainCamera.enabled)
+        {
+            mainCamera.enabled = false;
+            alternateCamera.enabled = true;
+        }
+    }
+}
