516cb23f063f50372db4ea4e9ba9db9e5e207fe6
Alexander Wittmann <alexwittmann01@gmail.com>
2018-04-29 12:43:03 -0600

    Changed mouse controls for sorting game

--- TheEscape/Assets/Scripts/PlayerController.cs
+++ TheEscape/Assets/Scripts/PlayerController.cs
@@ -396,11 +396,26 @@ public class PlayerController : MonoBehaviour {
 
             PauseScreenUI.gameObject.SetActive(isPaused);
         }
+        if (Input.GetMouseButtonDown(1))
+        {
+            Cursor.visible = !Cursor.visible;
+        }
+        if (Cursor.visible)
+        {
+            transform.Rotate(0, 0, 0);
+
+        }
+        if (!Cursor.visible)
+        {
+            transform.Rotate(0, Input.GetAxis("Mouse X") * sensitivity, 0);
+        }
+        /*
         if (Consumablecontact || otherContact || valuableContact || clothingcontact || Documentcontact )
         {
             transform.Rotate(0, 0, 0);
             Cursor.visible = true;
         }
+        */
 
         if (Input.GetKeyDown(KeyCode.Space) && isTalking)
         {
